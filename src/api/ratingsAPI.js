1983c0595a3cb3b1820ff1f2c196a7e3c2e3a75f
Davi Naizer <davi.naizer@gamesys.co.uk>
2022-09-26 21:25:02 +0100

    chore: remove logs

--- src/api/ratingsAPI.js
+++ src/api/ratingsAPI.js
@@ -1,6 +1,4 @@
 export function getEstablishmentRatings(pageNum = 1) {
-  console.log('🚀  ~ getEstablishmentRatings ~ pageNum', pageNum);
-
   return fetch(
     `http://api.ratings.food.gov.uk/Establishments/basic/${pageNum}/10`,
     { headers: { 'x-api-version': '2' } }
