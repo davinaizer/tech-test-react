52e7b9021e952687d6c2c7533dce81278f8fa5d5
Davi Naizer <davi.naizer@gamesys.co.uk>
2022-09-26 21:19:14 +0100

    refactor: adds default value for getEstablishmentRatings

--- src/api/ratingsAPI.js
+++ src/api/ratingsAPI.js
@@ -1,6 +1,8 @@
-export function getEstablishmentRatings(pageNum) {
+export function getEstablishmentRatings(pageNum = 1) {
+  console.log('🚀  ~ getEstablishmentRatings ~ pageNum', pageNum);
+
   return fetch(
     `http://api.ratings.food.gov.uk/Establishments/basic/${pageNum}/10`,
-    { headers: { "x-api-version": "2" } }
-  ).then((res) => res.json());
+    { headers: { 'x-api-version': '2' } }
+  ).then(res => res.json());
 }
