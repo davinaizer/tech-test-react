bf53d8d9ed1784e4b4fc375de0a14e376e186574
Davi Naizer <davi.naizer@gamesys.co.uk>
2022-09-26 21:23:57 +0100

    feat: adds fetching state logic for table rows

--- src/components/PaginatedEstablishmentsTable.js
+++ src/components/PaginatedEstablishmentsTable.js
@@ -1,72 +1,71 @@
-import React, { useState, useEffect } from "react";
-import { EstablishmentsTable } from "./EstablishmentsTable";
-import { EstablishmentsTableNavigation } from "./EstablishmentsTableNavigation";
-import { getEstablishmentRatings } from "../api/ratingsAPI";
+import React, { useState, useEffect } from 'react';
+import { EstablishmentsTable } from './EstablishmentsTable';
+import { EstablishmentsTableNavigation } from './EstablishmentsTableNavigation';
+import { getEstablishmentRatings } from '../api/ratingsAPI';
+import { LOADING_STATE } from '../constants';
 
 const tableStyle = {
-  background: "rgba(51, 51, 51, 0.9)",
-  padding: "10px",
-  width: "max-content",
-  marginLeft: "50px",
-  color: "white",
+  background: 'rgba(51, 51, 51, 0.9)',
+  padding: '10px',
+  width: 'max-content',
+  marginLeft: '50px',
+  color: 'white'
 };
 
 export const PaginatedEstablishmentsTable = () => {
   const [error, setError] = useState(null);
+  const [loadingState, setLoadingState] = useState(LOADING_STATE.idle);
   const [establishments, setEstablishments] = useState([]);
   const [pageNum, setPageNum] = useState(1);
   // eslint-disable-next-line no-unused-vars
   const [pageCount, setPageCount] = useState(100);
 
-  useEffect(() => {
+  const fetchEstablishments = () => {
+    setLoadingState(LOADING_STATE.loading);
+
     getEstablishmentRatings(pageNum).then(
-      (result) => {
+      result => {
+        setLoadingState(LOADING_STATE.idle);
         setEstablishments(result.establishments);
       },
-      (error) => {
+      error => {
+        setLoadingState(LOADING_STATE.error);
         setError(error);
       }
     );
-  }, []);
+  };
+
+  useEffect(() => {
+    fetchEstablishments();
+  }, [pageNum]);
 
   async function handlePreviousPage() {
     pageNum > 1 && setPageNum(pageNum - 1);
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
   }
 
   async function handleNextPage() {
     pageNum < pageCount && setPageNum(pageNum + 1);
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
   }
 
   if (error) {
     return <div>Error: {error.message}</div>;
-  } else {
-    return (
-      <div style={tableStyle}>
-        <h2>Food Hygiene Ratings</h2>
-        <EstablishmentsTable establishments={establishments} />
-        <EstablishmentsTableNavigation
-          pageNum={pageNum}
-          pageCount={pageCount}
-          onPreviousPage={handlePreviousPage}
-          onNextPage={handleNextPage}
-        />
-      </div>
-    );
   }
+
+  return (
+    <div style={tableStyle}>
+      <h2>Food Hygiene Ratings</h2>
+
+      <EstablishmentsTable
+        loadingState={loadingState}
+        establishments={establishments}
+      />
+
+      <EstablishmentsTableNavigation
+        pageNum={pageNum}
+        pageCount={pageCount}
+        onPreviousPage={handlePreviousPage}
+        onNextPage={handleNextPage}
+      />
+    </div>
+  );
 };
